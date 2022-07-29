d79444b0797eb44402c7130f1b9142ee956947a4
Ray Orolé <orole.ray@protonmail.com>
2022-07-29 21:49:49 +0200

    Custom toolbar

--- public/electron.js
+++ public/electron.js
@@ -1,36 +1,61 @@
-const path = require("path");
-const { app, BrowserWindow } = require("electron");
+const path = require('path');
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+const ipc = ipcMain;
 
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minHeight: 600,
+    minWidth: 800,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: false, // is default value after Electron v5
+      contextIsolation: true, // protect against prototype pollution
+      enableRemoteModule: false, // turn off remote
+      preload: path.join(__dirname, 'preload.js'), // use a preload script
     },
+    autoHideMenuBar: true,
+    frame: false,
+    icon: path.join(__dirname, 'logo192.png'),
   });
 
   // and load the index.html of the app.
   if (app.isPackaged) {
-    mainWindow.loadFile(path.join(__dirname, "index.html"));
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
   } else {
-    mainWindow.loadURL("http://localhost:3000");
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
   }
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // IPC events
+  ipc.on('minimize', () => {
+    mainWindow.minimize();
+  });
+
+  ipc.on('close', () => {
+    mainWindow.close();
+  });
+
+  ipc.on('maximize', () => {
+    if (mainWindow.isMaximized()) {
+      mainWindow.unmaximize();
+    } else {
+      mainWindow.maximize();
+    }
+  });
 }
 
 app.whenReady().then(createWindow);
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ETPIcon from '../assets/logo512.png';
+
+// Icons
+import { DuplicateIcon, XIcon, MinusIcon } from '@heroicons/react/outline';
+
+function MenuBar() {
+  return (
+    <div className="h-8 w-full items-center overflow-hidden bg-gray-700 justify-between flex left-0 top-0 pl-1 select-none menubar">
+      <div className="space-x-2 flex items-center flex-1">
+        <img alt="Icon" className="w-5 h-5" src={ETPIcon} />
+        <span className="text-gray-300 text-xs font-medium">
+          Etherspay Wallet
+        </span>
+      </div>
+      <div className="menubar-nodrag">
+        <button
+          onClick={() => window.electron.send('minimize')}
+          className="text-gray-300 hover:bg-gray-600 p-4 focus:border-none focus:ring-0 focus:outline-none"
+        >
+          <MinusIcon className="w-4 h-4" />
+        </button>
+        <button
+          onClick={() => window.electron.send('maximize')}
+          className="text-gray-300 hover:bg-gray-600 p-4 focus:border-none focus:ring-0 focus:outline-none"
+        >
+          <DuplicateIcon className="h-4 w-4" />
+        </button>
+        <button
+          onClick={() => window.electron.send('close')}
+          className="text-gray-300 hover:bg-red-500 p-4 focus:border-none focus:ring-0 focus:outline-none"
+        >
+          <XIcon className="w-4 h-4" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default MenuBar;
